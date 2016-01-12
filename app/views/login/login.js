e3b3166a620ce8e902f2d1585b5512fa9f3ee6e7
loghum <bramvanlochem@gmail.com>
2016-01-12 15:48:16 +0100

    login api beginsel

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -2,11 +2,22 @@
 var login = angular.module('myapp.login',['ngMaterial', 'ngRoute']);
 
 
-login.controller('loginController', function () {
+login.controller('loginController', function ($http) {
     var self = this;
 
-
     self.control = function () {
+        var parameter = JSON.stringify({username:self.name, password:self.name});
+        $http.post("api/zaxion.nl/login", parameter).
+        success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            window.alert("inlog juist");
+            console.log(data);
+        }).
+        error(function(data, status, headers, config) {
+            window.alert("inlog gevens zijn onjuist");
+        });
+
 
         if(self.name == "admin" && self.password == "admin") {
             document.location.href = "../../index.html"
