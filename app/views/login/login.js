c528820a433ccab2b8c037c54967e26e2a2bb922
loghum <bramvanlochem@gmail.com>
2016-01-13 08:42:26 +0100

    login href gemaakt

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,23 +1,35 @@
-'use strict';
-var login = angular.module('myapp.login',['ngMaterial', 'ngRoute']);
+
+var login = angular.module('myapp.login',['ngMaterial', 'ngRoute','ngStorage']);
 
 
-login.controller('loginController', function ($http) {
+login.controller('loginController', function ($http,$localStorage) {
     var self = this;
 
     self.control = function () {
-        var parameter = JSON.stringify({username:self.name, password:self.name});
-        $http.post("api/zaxion.nl/login", parameter).
+
+        var config = {headers:  {
+            "name": self.name,
+            "password": self.password,
+            "Content-Type": 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+        };
+        $http.get("http://zaxion.nl/api/login/", config).
         success(function(data, status, headers, config) {
             // this callback will be called asynchronously
             // when the response is available
             window.alert("inlog juist");
-            console.log(data);
+            window.alert(data);
+            self.save = function() {
+                $localStorage.token = data;
+            }
+            document.location.href = "../../index.html";
         }).
         error(function(data, status, headers, config) {
-            window.alert("inlog gevens zijn onjuist");
-        });
-        
-    }
+            window.alert("inlog onjuis");
+            window.alert(data);
+            window.alert(status);
+            window.alert(headers);
 
+        });
+    }
 });
