5b0977a1609e9a5764774f6897ed423f8a319fe4
jonathan <jonathan.bergers@gmail.com>
2016-01-16 12:09:49 +0100

    validtation, json get

--- app/views/gameEditor/gameEditor.js
+++ app/views/gameEditor/gameEditor.js
@@ -12,33 +12,63 @@ angular.module('myApp.gameEditor', ['ngRoute', 'ngMaterial'])
     /* FORM */
     .controller('inputController', [function () {
       var self = this;
-      self.game = function(){};
+      //self.game = function(){};
 
       self.game = {
-        mapbox_id: ''
+       title : null,
+        id: null,
+          punishTime: null
       };
+        /**checks if the user input is valid for sending
+         *
+         * @returns {boolean}
+         */
+        self.validInput = function(){
+
+            var title = self.game.title;
+            var id = self.game.id;
+            var punishTime = self.game.punishTime;
+
+            if(title == null || title.length == 0){
+                return false;
+            }
+            if(id == null || id.length == 0){
+                return false;
+            }
+            return !(punishTime == null || punishTime <= 0);
+
+
+
+    };
 
       self.sendGameData = function() {
           var jsonString = JSON.stringify(self.game);
-      }
+      };
 
       return true;
     }])
 
     /* TABLE */
-    .controller('tableController', [function () {
-      var self = this;
-      self.games = function(){};
+    .controller('tableController', ['$http', function ($http) {
 
-      self.games;
+        var self = this;
+        self.games = function(){};
+        self.games = [
+            {title: "test", id: "testId", punishTime: 10}
+        ];
 
-      self.getGames = function() {
+        var gamesEndpoint = "http://localhost:8888/games"
+        $http.get(gamesEndpoint).
+        success(function(data, status, headers, config) {
+            window.alert(data)
+            self.games = data
+        }).
+        error(function(data, status, headers, config) {
+            windows.alert(data + " " + status)
+            // log error
+        });
 
-      }
 
-      self.getJSON = function() {
-
-      }
 
       return true;
     }]);;
